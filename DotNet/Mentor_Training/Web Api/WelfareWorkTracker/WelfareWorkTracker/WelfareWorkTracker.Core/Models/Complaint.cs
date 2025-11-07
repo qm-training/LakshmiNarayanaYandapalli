a509da481a880dee598397c459432ebe058fd3b0
Lakshmi Narayana Yandapalli <ylnarayana@qualminds.com>
2025-11-07 03:54:33 -0600

    Models and DbContext imported and modified

---
+++ DotNet/Mentor_Training/Web Api/WelfareWorkTracker/WelfareWorkTracker/WelfareWorkTracker.Core/Models/Complaint.cs
@@ -0,0 +1,38 @@
+﻿namespace WelfareWorkTracker.Core.Models;
+public class Complaint
+{
+    public int ComplaintId { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public string CountryName { get; set; } = null!;
+
+    public string StateName { get; set; } = null!;
+
+    public string DistrictName { get; set; } = null!;
+
+    public string ConstituencyName { get; set; } = null!;
+
+    public string VillageName { get; set; } = null!;
+
+    public int Pincode { get; set; }
+
+    public string Address { get; set; } = null!;
+
+    public int Attempts { get; set; }
+
+    public bool IsClosed { get; set; }
+
+    public int CitizenId { get; set; }
+
+    public int LeaderId { get; set; }
+
+    public DateTime DateCreated { get; set; }
+
+    public DateTime DateUpdated { get; set; }
+
+    public int ReferenceNumber { get; set; }
+
+}
---
+++ DotNet/Mentor_Training/Web Api/WelfareWorkTracker/WelfareWorkTracker/WelfareWorkTracker.Core/Models/DailyComplaint.cs
@@ -0,0 +1,18 @@
+﻿namespace WelfareWorkTracker.Core.Models;
+public class DailyComplaint
+{
+    public int DailyComplaintId { get; set; }
+
+    public int ConstituencyId { get; set; }
+
+    public int LeaderId { get; set; }
+
+    public bool IsCompleted { get; set; }
+
+    public DateTime DateCreated { get; set; }
+
+    public DateTime DateUpdated { get; set; }
+
+    public virtual Constituency Constituency { get; set; } = null!;
+
+}
